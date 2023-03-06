eed1dd0746f4df9533c977d88d7e6524ed9b1a12
degabaia <degabaia@D250-S18>
2023-03-06 15:48:03 +0100

    classes etape + concerne (hihihiha)

--- WS_VINOTRIP/Models/EntityFramework/Concerne.cs
+++ WS_VINOTRIP/Models/EntityFramework/Concerne.cs
@@ -1,6 +1,29 @@
-﻿namespace WS_VINOTRIP.Models.EntityFramework
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WS_VINOTRIP.Models.EntityFramework
 {
+    [Table("t_j_concerne_ccr")]
     public class Concerne
     {
+        [Key]
+        [Column("etp_id")]
+        public int EtapeId { get; set; }
+
+        [Key]
+        [Column("ele_id")]
+        public int ElementId { get; set; }
+        
+        [Column("ccr_horaire", TypeName = "time")]
+        [Required]
+        public DateTime Horaire { get; set; }
+
+        [ForeignKey("EtapeId")]
+        [InverseProperty("ConcerneEtape")]
+        public virtual Sejour EtapeConcerne { get; set; }
+
+        [ForeignKey("ElementId")]
+        [InverseProperty("ConcerneElementEtape")]
+        public virtual Sejour ElemenEtapeConcerne{ get; set; }
     }
 }
--- WS_VINOTRIP/Models/EntityFramework/Etape.cs
+++ WS_VINOTRIP/Models/EntityFramework/Etape.cs
@@ -1,6 +1,33 @@
-﻿namespace WS_VINOTRIP.Models.EntityFramework
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WS_VINOTRIP.Models.EntityFramework
 {
+    [Table("t_e_etape_etp")]
     public class Etape
     {
+        [Key]
+        [Column("etp_id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EtapeId { get; set; }
+
+        [Key]
+        [Column("sjr_id")]
+        public int SejourId { get; set; }
+
+        [Column("etp_titreetape")]
+        [Required]
+        [StringLength(100)]
+        public string? TitreEtape { get; set; }
+
+        [Column("etp_descriptionetape", TypeName = "text")]
+        public string? DescriptionEtape { get; set; }
+
+        [ForeignKey("SejourId")]
+        [InverseProperty("EtapesSejour")]
+        public virtual Sejour SejourEtape { get; set; }
+
+        [InverseProperty("EtapeConcerne")]
+        public virtual ICollection<Concerne>? ConcerneEtape { get; set; }
     }
 }
