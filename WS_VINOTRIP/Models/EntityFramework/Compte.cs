0edca26732153c5f1315d84c714f8cf528e03e1b
rivalinn <rivalinn@D250-S16>
2023-03-06 15:05:09 +0100

    Classe Compte

--- WS_VINOTRIP/Models/EntityFramework/Compte.cs
+++ WS_VINOTRIP/Models/EntityFramework/Compte.cs
@@ -1,6 +1,43 @@
-﻿namespace WS_VINOTRIP.Models.EntityFramework
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace WS_VINOTRIP.Models.EntityFramework
 {
-    public class Compte
+    [Table("t_e_compte_cmp")]
+    public partial class Compte
     {
+        [Key]
+        [Column("cmp_id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdCompte { get; set; }
+
+        [Key]
+        [Column("mdp_id")]
+        public int IdMdp { get; set; }
+
+        [Key]
+        [Column("tpc_id")]
+        public int IdTypeCompte { get; set; }
+
+        [Key]
+        [Column("prs_id")]
+        public int IdPersonne { get; set; }
+
+        [Column("cmp_telcompte", TypeName = "char(10)")]
+        [RegularExpression(@"^0[0-9]{9}$", ErrorMessage = "…")]
+        public string? TelCompte { get; set; }
+
+        [Column("cmp_newsletter")]
+        public bool Newsletter { get; set; }
+
+        [Column("cmp_estverifie")]
+        public bool EstVerifie { get; set; }
+
+        [Column("cmp_estadmin")]
+        public bool EstAdmin { get; set; }
+
+        [Column("cmp_dateconnexion", TypeName = "date")]
+        public DateTime DateConnexion { get; set; }
     }
 }
