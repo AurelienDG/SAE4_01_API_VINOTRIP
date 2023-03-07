bc53490fbe1db0e00a45485e26b138f8a3a82aab
rivalinn <rivalinn@D250-S16>
2023-03-07 10:32:17 +0100

    Modif : SejourCadeau

--- WS_VINOTRIP/Models/EntityFramework/SejourCadeau.cs
+++ WS_VINOTRIP/Models/EntityFramework/SejourCadeau.cs
@@ -1,6 +1,30 @@
-﻿namespace WS_VINOTRIP.Models.EntityFramework
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WS_VINOTRIP.Models.EntityFramework
 {
-    public class SejourCadeau
+    [Table("t_e_Sejourcadeau_sjc")]
+    public partial class SejourCadeau
     {
+        [Required]
+        [Column("bcd_id")]
+        public int BonCadeauId { get; set; }
+
+        [Required]
+        [Column("sjr_id")]
+        public int SejourId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Column("sjc_choixdestination")]
+        public string? ChoixDestination { get; set; }
+
+        [ForeignKey("BonCadeauId")]
+        [InverseProperty("SejourCadeauBonCadeau")]
+        public virtual BonCadeau BonCadeauSejourCadeau { get; set; } = null!;
+
+        [ForeignKey("SejourId")]
+        [InverseProperty("SejourCadeauSejour")]
+        public virtual Sejour SejourSejourCadeau{ get; set; } = null!;
     }
 }
