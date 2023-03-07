5522611c2786a6866e9c7489457bb21bc59bc75c
rivalinn <rivalinn@D250-S16>
2023-03-07 10:35:51 +0100

    Modif : SejourCadeau Ajout : ChequeCadeau

---
+++ WS_VINOTRIP/Models/EntityFramework/ChequeCadeau.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WS_VINOTRIP.Models.EntityFramework
+{
+    [Table("t_e_chequecadeau_cqc")]
+    public partial class ChequeCadeau
+    {
+        [Required]
+        [Column("bcd_id")]
+        public int BonCadeauId { get; set; }
+
+        [Required]
+        [Column("cqc_montant")]
+        public int montant { get; set; }
+
+        [ForeignKey("BonCadeauId")]
+        [InverseProperty("ChequeCadeauBonCadeau")]
+        public virtual BonCadeau BonCadeauChequeCadeau { get; set; } = null!;
+    }
+}
