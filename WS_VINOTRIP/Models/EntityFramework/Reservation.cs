3d7f0a488a7a1c84232dcfd49e656e3e38a030b9
degabaia <degabaia@D250-S18>
2023-03-09 08:46:31 +0100

    oui

---
+++ WS_VINOTRIP/Models/EntityFramework/Reservation.cs
@@ -0,0 +1,12 @@
+﻿namespace WS_VINOTRIP.Models.EntityFramework
+{
+    public partial class Reservation
+    {
+        public int ReservationId { get; set; }
+        public int SejourId { get; set; }
+        public int NbAdultes { get; set; }
+        public int NbEnfants { get; set; }
+        public int NbChambres { get; set; }
+        public double MyProperty { get; set; }
+    }
+}
