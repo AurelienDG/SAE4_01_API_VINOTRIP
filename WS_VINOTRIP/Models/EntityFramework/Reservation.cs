72c370571b19a435acada96a92c3a392b78e8f50
degabaia <degabaia@D250-S18>
2023-03-09 09:38:25 +0100

    Cellian + Irwin contents (mais pas trop Cellian quand même)

--- WS_VINOTRIP/Models/EntityFramework/Reservation.cs
+++ WS_VINOTRIP/Models/EntityFramework/Reservation.cs
@@ -6,44 +6,34 @@ namespace WS_VINOTRIP.Models.EntityFramework
     [Table("t_e_reservation_rsv")]
     public partial class Reservation
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("rsv_id")]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity), Column("rsv_id")]
         public int ReservationId { get; set; }
 
-        [Column("sjr_id")]
-        [Required]
+        [Required, Column("sjr_id")]
         public int SejourId { get; set; }
-
-        [Column("rsv_nbadultes")]
-        [Required]
-        public int NbAdultes { get; set; }
-
-        [Column("rsv_nbenfants")]
-        [Required]
-        public int NbEnfants { get; set; }
-
-        [Column("rsv_nbchambres")]
-        [Required]
-        public int NbChambres { get; set; }
-
-        [Column("rsv_id", TypeName = "numeric(9,2)")]
-        [Required]
-        public double PrixTotal { get; set; }
-
-        [Column("rsv_datedebutresa", TypeName = "date")]
-        [Required]
-        public DateTime DateDebutResa{ get; set; }
-
-        [Column("rsv_datefinresa", TypeName = "date")]
-        [Required]
-        public DateTime DateFinResa{ get; set; }
 
         [Column("bcd_id")]
         public int BonCadeauId { get; set; }
 
-        [Column("rsv_datefacture")]
-        [Required]
+        [Required, Column("rsv_nbadultes")]
+        public int NbAdultes { get; set; }
+
+        [Required, Column("rsv_nbenfants")]
+        public int NbEnfants { get; set; }
+
+        [Required, Column("rsv_nbchambres")]
+        public int NbChambres { get; set; }
+
+        [Required, Column("rsv_id", TypeName = "numeric(9,2)")]
+        public double PrixTotal { get; set; }
+
+        [Required, Column("rsv_datedebutresa", TypeName = "date")]
+        public DateTime DateDebutResa{ get; set; }
+
+        [Required, Column("rsv_datefinresa", TypeName = "date")]
+        public DateTime DateFinResa{ get; set; }
+
+        [Required, Column("rsv_datefacture")]
         public DateTime DateFacture { get; set; }
 
         [ForeignKey("SejourId")]
