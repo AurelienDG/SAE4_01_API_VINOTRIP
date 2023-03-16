8298f6a09a9adc90ffd8646fd0bdc75f0a0a6ee6
Cessouille <celian.chausson@etu.univ-smb.fr>
2023-03-16 08:42:15 +0100

    Vignoble

--- WS_VINOTRIP/Program.cs
+++ WS_VINOTRIP/Program.cs
@@ -26,6 +26,10 @@ builder.Services.AddDbContext<VinotripDBContext>(options => options.UseNpgsql(bu
 
 builder.Services.AddScoped<IDataRepository<Sejour>, SejourManager>();
 builder.Services.AddScoped<IDataRepository<Comporte>, ComporteManager>();
+builder.Services.AddScoped<IDataRepository<Avis>, AvisManager>();
+builder.Services.AddScoped<IDataRepository<RouteDesVins>, RoutesDesVinsManager>();
+builder.Services.AddScoped<IDataRepository<User>, UserManager>();
+builder.Services.AddScoped<IDataRepository<Vignoble>, VignobleManager>();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 .AddJwtBearer(options =>
